74b7d7b8e51195a6892a7ea745eb6693d2c62680
bohdanchevuck <b.chevuck@gmail.com>
2018-04-12 15:25:15 +0300

    edited

--- form.js
+++ form.js
@@ -1,8 +1,7 @@
 src="form.js"
 function checkEmail() {
   var emailAdress = document.getElementById('checkEmail').value;
-  var validEmail =  /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-//початок від однієї букви до нескінченості, - + _, @, від онднієї букви до нескінченості
+  var validEmail = /^[\w\.\d-_]+@[\w\.\d-_]+\.\w{2,4}$/i;//початок від однієї букви до нескінченості, - + _, @, від онднієї букви до нескінченості
 if (validEmail.test(emailAdress) === false) {  
       var error = generateError('Поле повинне містити символ @ і мати коректне доменне імя')
       email.parentElement.insertBefore(error, email)
@@ -11,3 +10,4 @@ if (validEmail.test(emailAdress) === false) {
   }
   return true;
 }
+pattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
